4e82c9e83ddc93a6fb5c2629930b689b3f6053b6
Shawn Atwood <sdatwood4@gmail.com>
2025-03-17 22:51:54 -0400

    first commit

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,12 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+--
+
+-- Keybinding for ToggleTerm floating terminal
+--vim.keymap.del("n", "<leader>fT")
+--vim.keymap.del("n", "<C-/>")
+--vim.keymap.set("n", "<leader>ft", "<cmd>ToggleTerm <CR>", { desc = "Toggle floating terminal" })
+--vim.keymap.set("n", "<C-/>", "<cmd>ToggleTerm <CR>", { desc = "Toggle floating terminal" })
+--vim.keymap.set("n", "<C-_>", "<cmd>ToggleTerm<CR>", { desc = "Toggle floating terminal" })
+--vim.keymap.set("t", "<C-/>", "<cmd>ToggleTerm <CR>", { desc = "Toggle floating terminal" })
